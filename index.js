f24ec36545f054fee270819c92723721b32d17b0
minh <minhnvgch200213@fpt.edu.vn>
2022-10-01 14:14:39 +0700

    ok

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000
+
+const path = require("path");
+app.set("views", path.join(__dirname, "views"));
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/about", (req, res) => {
+    res.render("about")
+})
+
+app.listen(port)
